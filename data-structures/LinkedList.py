6095c58a96e35a6a81ae25905defb08d8a2d0fb3
Inkollu <akashdhar.apssdc@gmail.com>
2024-05-23 13:21:46 +0530

    linked list added delete_from_beginning, delete_from_last, sort_insert, reverse

--- data-structures/LinkedList.py
+++ data-structures/LinkedList.py
@@ -21,19 +21,61 @@ class LinedList:
         return curr
 
     def insert_at_front(self, data):
+        temp = Node(data)
         if self.head is None:
-            self.head = Node(data)
+            self.head = temp
             return self.head
         else:
-            temp = Node(data)
             temp.next = self.head
             return temp
+
+    def insert_at_position(self, pos, data):
+        temp = Node(data)
+        if pos == 1:
+            temp.next = self.head
+            return temp
+        curr = self.head
+        for i in range(pos - 2):
+            curr = curr.next
+            if curr is None:
+                return self.head
+        temp.next = curr.next
+        curr.next = temp
+        return self.head
 
     def delete(self, data):
         pass
 
     def delete_from_beginning(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            return self.head.next
+
+    def delete_from_last(self):
+        if self.head is None:
+            return None
+        if self.head.next is None:
+            return None
+        curr = self.head
+        while curr.next.next is not None:
+            curr = curr.next
+        curr.next = None
+
+    def sort_insert(self, data):
+        temp = Node(data)
+        if self.head is None:
+            return temp
+        elif data < self.head.data:
+            temp.next = self.head
+            return temp
+        else:
+            curr = self.head
+            while curr.next is not None and curr.next.data < data:
+                curr = curr.next
+            temp.next = curr.next
+            curr.next = temp
+            return self.head
 
     def search(self, data):
         curr = self.head
@@ -41,3 +83,16 @@ class LinedList:
             if curr.data == data:
                 return curr
         return None
+
+    def reverse(self):
+        stack = []
+        curr = self.head
+        while curr is not None:
+            stack.append(curr.data)
+            curr = curr.next
+        curr = self.head
+        while curr is not Node:
+            curr.data = stack.pop()
+            curr = curr.next
+        return self.head
+
